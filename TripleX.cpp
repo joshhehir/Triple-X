31bd5898ed273f325277d677392ea2bf082a2bf6
JoshH <67282489+joshhehir@users.noreply.github.com>
2020-11-16 23:25:42 +1100

    Updates made
    almost complete need to finish Rand functions

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,24 +1,65 @@
 #include <iostream>
 
-int main()
-{   
-    std::cout << std::endl;
-    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
+void PrintIntroduction(int Difficulty)
+{
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure server room...\nEnter the correct code to continue...\n\n";
+}
 
-    const int CodeA = 4, CodeB = 3, CodeC = 2;
+bool PlayGame(int Difficulty)
+{
+    PrintIntroduction(Difficulty);
+
+    const int CodeA = rand(), CodeB = rand(), CodeC = rand();
 
     const int CodeSum = CodeA + CodeB + CodeC; 
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std:: cout << std:: endl;
-    std:: cout << "There are 3 numbers in the code" << std:: endl;
-    std:: cout << "The codes add-up to: "<< CodeSum << std::endl;
-    std:: cout << "The codes multiply to give: " << CodeProduct << std::endl;
+    //Print CodeSum and CodeProduct to the terminal
+    std:: cout << "\nThere are 3 numbers in the code";
+    std:: cout << "\nThe codes add-up to: "<< CodeSum;
+    std:: cout << "\nThe codes multiply to give: " << CodeProduct << std::endl;
 
-    int PlayerGuess;
-    std::cin >> PlayerGuess;
-    std::cout << "You entered: " << PlayerGuess;
+    //Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+    std::cout << "You entered: " << GuessA << GuessB << GuessC << std:: endl;
+    
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
 
+    //Check if the players guess is correct
+    if(GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "\n*** Well done agent! You have extracted a file! Keep going! ****";
+        return true;
+    }
+    else 
+    {
+        std::cout << "\n*** You entered the wrong code! Careful agent! Try again! ***";
+        return false;
+    }
+}
+
+int main()
+{
+    int LevelDifficulty = 1;
+    int const MaxDifficulty = 5;
+
+    while(LevelDifficulty <= MaxDifficulty)
+    { 
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); //Clears any errors
+        std::cin.ignore(); //Discards the buffer
+
+        if(bLevelComplete) 
+        {
+            //increase the level difficulty
+            ++LevelDifficulty;
+        }
+    }
+    std::cout << "\n*** Great work Agent! you got all the files! Now get out of there! ***\n";
     return 0;
 }
